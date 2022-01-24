b47081a516aa5922f94c0d754a900bcf8b6eec74
Jakob Schlager <jakob.schlager.biz@gmail.com>
2022-01-24 11:28:18 +0100

    add mailsettings in appsettings.json

---
+++ NotificationService/NotificationService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace NotificationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            EmailClient.SendEmail(); 
+
+            return null; 
+        }
+    }
+}
